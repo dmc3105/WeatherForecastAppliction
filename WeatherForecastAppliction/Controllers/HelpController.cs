ccb86f3ad1eee1a421ea3bb61971ed29d1b61170
Alexey Govorov <algovor04@gmail.com>
2026-09-17 20:51:52 +0700

    Add help controller

---
+++ WeatherForecastAppliction/Controllers/HelpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecastAppliction.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HelpController : ControllerBase
+    {
+
+        public Dictionary<string, object> help()
+        {
+            return new Dictionary<string, object> { { "status", "ok" } };
+        }
+    }
+}
